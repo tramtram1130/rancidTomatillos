b2965e195ec0928c63ea16a0db299254a38ed931
Dana Truong <tramtramphotos@gmail.com>
2022-12-02 16:01:46 -0600

    Revert Card class component back to functional component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,26 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Card.css'
 
-class Card extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      id: props.id,
-      posterPath: props.posterPath
-    }
-  }
-
-  handleClick = (event) => {
-    this.props.viewMovie(event.target.id)
-  }
-  
-  render() {
-    return (
-      <div className='card-container'>
-        <img className='card' id={this.state.id} onClick={() => this.props.viewMovie(this.state.id)} src={this.state.posterPath} />
-      </div>
-    )
-  }
+const Card = (props) => {
+  return (
+    <div className='card-container'>
+      <img className='card' id={props.id} onClick={() => props.viewMovie(props.id)} src={props.posterPath} />
+    </div>
+  )
 }
 
 export default Card
