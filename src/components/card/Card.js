664f55cc6d9afbf80b4df80453a6021c44df12c8
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-12-07 18:57:58 -0600

    Added navlink to card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Card.css'
-
+import { NavLink } from 'react-router-dom'
 const Card = (props) => {
   return (
+    <NavLink to={`/${props.id}`}>
     <div className='card-container'>
-      <img className='card' id={props.id} onClick={() => props.viewMovie(props.id)} src={props.posterPath} />
+      <img className='card' id={props.id} src={props.posterPath} />
     </div>
+    </NavLink>
   )
 }
-
 export default Card
