f9a1c727fc99a795044c929fa3fe0ae198673d73
Dana Truong <tramtramphotos@gmail.com>
2022-12-07 18:41:04 -0600

    Update the index file to import React Router and wrap it around App

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import Header from './components/Header/Header';
 import App from './components/App/App';
 import Footer from './components/Footer/Footer'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <Header />
     <App />
     <Footer />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
